dabbab6dc49b8b13dd7c023990abf4f14b46ad4e
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-05-20 16:49:30 +0900

    Initial Commit

---
+++ freee_SelectionSubject_AccountBookTests/freee_SelectionSubject_AccountBookTests.swift
@@ -0,0 +1,34 @@
+//
+//  freee_SelectionSubject_AccountBookTests.swift
+//  freee_SelectionSubject_AccountBookTests
+//
+//  Created by 大林拓実 on 2019/05/20.
+//  Copyright © 2019 TakumiObayashi. All rights reserved.
+//
+
+import XCTest
+@testable import freee_SelectionSubject_AccountBook
+
+class freee_SelectionSubject_AccountBookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
